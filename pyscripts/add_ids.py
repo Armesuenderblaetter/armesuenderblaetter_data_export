c7638280367a41cf4d66b0690ecc10d010655307
cfhaak <c.f.haak@posteo.de>
2024-04-08 14:45:32 +0200

    adding ids to l/p, see #16

---
+++ pyscripts/add_ids.py
@@ -0,0 +1,23 @@
+import glob
+from acdh_tei_pyutils.tei import TeiReader, ET
+
+xml_path = "./todesurteile_master/303_annot_tei/*.xml"
+
+
+def create_ids_for_element(doc: TeiReader, element_name: str):
+    doc_id = xml_doc.any_xpath('//tei:TEI/@xml:id')[0]
+    xml_namespace = f'{{{xml_doc.ns_xml.get("xml")}}}'
+    i = 0
+    for element in doc.any_xpath(f"//tei:body//tei:{element_name}"):
+        i += 1
+        element_id = doc_id + "_" + element_name + "{:04}".format(i)
+        element.set(xml_namespace + "id", element_id)
+
+
+print("Adding ids to p and l emlements")
+for xml_filepath in glob.glob(xml_path):
+    xml_doc = TeiReader(xml_filepath)
+    create_ids_for_element(xml_doc, "p")
+    create_ids_for_element(xml_doc, "l")
+    xml_doc.tree_to_file(xml_filepath)
+print("Done")
