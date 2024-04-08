4f0890a2d32980ed0e3992042ed2d53998f241b1
cfhaak <c.f.haak@posteo.de>
2024-04-08 15:22:39 +0200

    now adding lg/p/l with all atribs, fixed nesting; see #16

--- pyscripts/mk_verticals.py
+++ pyscripts/mk_verticals.py
@@ -7,9 +7,16 @@ from acdh_tei_pyutils.utils import extract_fulltext
 
 full_xml_ns = "{http://www.w3.org/1999/xhtml}"
 full_tei_ns = "{http://www.tei-c.org/ns/1.0}"
+xml_id_name = '{http://www.w3.org/XML/1998/namespace}id'
+
+def get_id(element):
+    return element.get(
+        xml_id_name,
+        ""
+    )
 
 
-def mk_vertical_from_w(teiw_tag, full_xml_ns):
+def mk_vertical_from_w(teiw_tag):
     # extractable attribute:
     # lemma, ana, pos, id, join, part
     lem_tag = teiw_tag.xpath(
@@ -39,10 +46,7 @@ def mk_vertical_from_w(teiw_tag, full_xml_ns):
     lemma = teiw_tag.get('lemma', "no")
     ana = teiw_tag.get('ana', "no")
     pos = teiw_tag.get('pos', "no")
-    element_id = teiw_tag.get(
-        '{http://www.w3.org/XML/1998/namespace}id',
-        ""
-    )
+    element_id = get_id(teiw_tag)
     join = teiw_tag.get('join', "no")
     part = teiw_tag.get('part', "no")
     vertical = '\t'.join(
@@ -58,6 +62,27 @@ def mk_vertical_from_w(teiw_tag, full_xml_ns):
     )
     return vertical
 
+def process_lg(lg_element, doc_verticals, nsmap):
+    doc_verticals.append(
+        f"<lg type='{lg_element.get('type', '')}'>"
+    )
+    for sub_lg in lg_element.xpath("./tei:lg", namespaces=nsmap):
+        process_lg(
+            sub_lg,
+            doc_verticals,
+            nsmap
+        )
+    for l_element in lg_element.xpath("./tei:l", namespaces=nsmap):
+        doc_verticals.append(
+            f"<l id={get_id(l_element)}>"
+        )
+        for w in l_element.xpath(".//tei:w", namespaces=nsmap):
+            vertical = mk_vertical_from_w(w)
+            doc_verticals.append(vertical)
+        doc_verticals.append("</l>")
+    doc_verticals.append("</lg>")
+    return doc_verticals
+
 
 def export_verticals_from_doc(
     doc: TeiReader,
@@ -69,45 +94,29 @@ def export_verticals_from_doc(
     open_doc_vertical = f"""<doc id="{doc_id}" attrs="word lemma \\\
         ana pos id join part" title="{title}" date="{date}">"""
     close_doc_vertical = '</doc>\n'
-    p_vertical_open = '<p>'
-    p_vertical_closed = '</p>'
-    lg_vertical_open = "<lg>"
-    lg_vertical_close = "</lg>"
-    l_vertical_open = "<l>"
-    l_vertical_close = "</l>"
     doc_verticals.append(open_doc_vertical)
-    structure_elements = doc.any_xpath("//tei:p|//tei:lg")
+    structure_elements = doc.any_xpath("//tei:p|//tei:lg[not(parent::tei:lg)]")
     if len(structure_elements) != 0:
         for sel in structure_elements:
             sel_name = sel.xpath("local-name()")
             if sel_name == "p":
-                doc_verticals.append(p_vertical_open)
+                doc_verticals.append(
+                    f"<p id={get_id(sel)}>"
+                )
                 for w in sel.xpath(".//tei:w", namespaces=doc.nsmap):
-                    vertical = mk_vertical_from_w(
-                        w,
-                        full_xml_ns=full_xml_ns
-                    )
+                    vertical = mk_vertical_from_w(w)
                     doc_verticals.append(vertical)
-                doc_verticals.append(p_vertical_closed)
+                doc_verticals.append('</p>')
             elif sel_name == "lg":
-                doc_verticals.append(lg_vertical_open)
-                for l_element in sel.xpath("./tei:l", namespaces=doc.nsmap):
-                    doc_verticals.append(l_vertical_open)
-                    for w in l_element.xpath(".//tei:w", namespaces=doc.nsmap):
-                        vertical = mk_vertical_from_w(
-                            w,
-                            full_xml_ns=full_xml_ns
-                        )
-                        doc_verticals.append(vertical)
-                    doc_verticals.append(l_vertical_close)
-                doc_verticals.append(lg_vertical_close)
+                doc_verticals = process_lg(
+                    lg_element=sel,
+                    doc_verticals=doc_verticals,
+                    nsmap=doc.nsmap
+                )
     else:
         input(doc_id)
         for w in doc.any_xpath(".//tei:w"):
-            vertical = mk_vertical_from_w(
-                w,
-                full_xml_ns=full_xml_ns
-            )
+            vertical = mk_vertical_from_w(w)
             doc_verticals.append(vertical)
     doc_verticals.append(close_doc_vertical)
     return "\n".join(doc_verticals)
@@ -130,10 +139,7 @@ def get_verticals_from_xml_files(input_filepath):
         for teiw_tag in teiw_tags:
             full_xml_ns = "{"+doc.ns_xml["xml"]+"}"
             doc_verticals.append(
-                mk_vertical_from_w(
-                    teiw_tag,
-                    full_xml_ns=full_xml_ns
-                )
+                mk_vertical_from_w(teiw_tag)
             )
         verticals.append(
             (
@@ -165,12 +171,3 @@ def prepare_output_dir(output_filepath="./out/"):
     print(f"output to {output_dir}")
     os.makedirs(output_dir, exist_ok=True)
     return output_dir
-
-
-# if __name__ == "__main__":
-#     input_filepath = "./todesurteile_master/303_annot_tei/"
-#     output_dir = prepare_output_dir()
-#     verticals = get_verticals_from_xml_files(
-#         input_filepath
-#     )
-#     write_verticals_to_file(verticals, output_dir)
