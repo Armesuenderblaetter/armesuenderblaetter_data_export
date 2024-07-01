78337525c7a15c3401858fda0573bc9bc3c94279
cfhaak <c.f.haak@posteo.de>
2024-07-01 11:10:58 +0200

    removed mentions-to-indices, rather do it in static

--- shellscripts/extract_infos.sh
+++ shellscripts/extract_infos.sh
@@ -4,4 +4,4 @@ pip install -r ./pyscripts/requirements.txt
 python ./pyscripts/extract_data.py
 person_index_path="./out/xml/indices/*.xml"
 editions_path="./out/xml/editions/*.xml"
-mentions-to-indices -t "erwähnt in " -i "$person_index_path" -f "$editions_path" --title-xpath "//tei:title/text()"
+# mentions-to-indices -t "erwähnt in " -i "$person_index_path" -f "$editions_path" --title-xpath "//tei:title/text()"
