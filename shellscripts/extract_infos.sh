4b2f65672fcb900a99feb8d7b97529611bdbc0db
cfhaak <c.f.haak@posteo.de>
2024-07-08 12:30:55 +0200

    removed unused shellscript

--- shellscripts/extract_infos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# installs requirements and runs script(s) to extract infos from xmls
-pip install -r ./pyscripts/requirements.txt
-python ./pyscripts/extract_data.py
-person_index_path="./out/xml/indices/*.xml"
-editions_path="./out/xml/editions/*.xml"
-# mentions-to-indices -t "erwähnt in " -i "$person_index_path" -f "$editions_path" --title-xpath "//tei:title/text()"
