2c27695848dfb99cdb0506765145a5e0b7e9908e
cfhaak <c.f.haak@posteo.de>
2024-07-09 11:52:42 +0200

    added label translator see https://github.com/Flugblatter/flugblaetter_static/issues/38

---
+++ pyscripts/label_translator.py
@@ -0,0 +1,12 @@
+label_dict = {
+    "m": "männlich",
+    "f": "weiblich",
+    "unwed": "ledig",
+    "married": "verheirated",
+    "widowed": "verwitwet",
+    # "cath": "katholische Kirche",
+    # "cal": "reformierte Kirche"
+    "cath": "katholisch",
+    "cal": "reformiert",
+    "jud": "jüdisch",
+}
