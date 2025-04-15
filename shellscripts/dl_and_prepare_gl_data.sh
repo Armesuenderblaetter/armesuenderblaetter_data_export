69261fc6279a88cf83629d5884f79c3a5f7a6a5f
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-04-15 14:45:39 +0200

    gl_token

--- shellscripts/dl_and_prepare_gl_data.sh
+++ shellscripts/dl_and_prepare_gl_data.sh
@@ -16,11 +16,12 @@ custom_output_dir="./todesurteile_master/"
 # download and unzip
 # rm old data / create target dir
 if [ -d $custom_output_dir ]; then rm -r $custom_output_dir; fi
-mkdir $custom_output_dir
+mkdir -p $custom_output_dir
 # to get an overview of what you could do with API/Token, use:
 # curl "${gitlab_api_base_url}?private_token=${gitlab_token}"
 # get the data via api
-curl "${gitlab_api_base_url}${gitlab_project_id}${gitlab_api_request}private_token=${GITLAB_SOURCE_TOKEN}" --output $output_archive
+curl --header "PRIVATE-TOKEN: ${GITLAB_SOURCE_TOKEN}" "${gitlab_api_base_url}${gitlab_project_id}${gitlab_api_request}" --output $output_archive
+# curl "${gitlab_api_base_url}${gitlab_project_id}${gitlab_api_request}private_token=${GITLAB_SOURCE_TOKEN}" --output $output_archive
 # find out, how the api named the parent folder in the archive
 top_level_folder_name="./"`tar -tzf $output_archive | head -1 | cut -f1 -d"/"`
 # unzip the archive
@@ -37,6 +38,6 @@ fi
 if [ -f $output_archive ]; then rm $output_archive; fi
 
 # rename some files for arche/processing
-python3 ./pyscripts/renameFiles.py
+pyscripts/renameFiles.py
 add-attributes -g $custom_output_dir"/303_annot_tei/*.xml" -b "."
-python3 ./pyscripts/add_ids.py
+pyscripts/add_ids.py
